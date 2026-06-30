3366cbc3ab256ff50ad53d721fd62c119853292f
CalebMostyn <calebmostyn@gmail.com>
2026-06-29 21:44:43 -0500

    Fixture for File Management

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import subprocess
 import time
 
@@ -6,10 +8,9 @@ import requests
 
 from utils.constants import *
 
-@pytest.fixture
-def server():
-    server = subprocess.Popen([SERVER_BINARY, "-f", "tasks.txt"])
-    time.sleep(1)
+@pytest.fixture(scope="module")
+def server(request, task_file):
+    server = subprocess.Popen([SERVER_BINARY, "-f", task_file])
 
     response = None
     while not response or response.status_code != 200:
@@ -20,6 +21,7 @@ def server():
             )
         time.sleep(0.1)
         try:
+            # send server requests until the default endpoint ('/') responds 200
             response = requests.get(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}")
         except Exception:
             pass
@@ -27,3 +29,24 @@ def server():
     yield server
 
     server.kill()
+    server.wait()
+
+@pytest.fixture(scope="module")
+def task_file(request):
+    filepath = request.param
+
+    file_existed_already = False
+    if os.path.isfile(filepath):
+        file_existed_already = True
+        # copy to tmp
+        shutil.copy(filepath, f"/tmp/{os.path.basename(filepath)}")
+
+    yield filepath
+
+    if file_existed_already:
+        # return file to version prior to test
+        shutil.copy(f"/tmp/{os.path.basename(filepath)}", filepath)
+    elif os.path.isfile(filepath):
+        # delete leftover file
+        os.remove(filepath)
+
