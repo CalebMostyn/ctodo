37799121630d1807b8e1c7038950b5cea777b3a9
CalebMostyn <calebmostyn@gmail.com>
2026-06-29 23:37:06 -0500

    Testing Multiple Task POSTs

--- test/integration/test_tasks.py
+++ test/integration/test_tasks.py
@@ -1,29 +1,56 @@
+import time
+
 import pytest
 import requests
 
 from utils.constants import *
 
 @pytest.mark.parametrize("task_file", ["test_files/blank_tasks.json", "nonexistent_tasks.json"], indirect=True)
-class TestTasks:
+class TestAddTasks:
 
     task_file = "blank_tasks.txt"
-    test_task_title = "test task"
+    test_task_title = "test task : "
 
-    def test_get_blank_tasks(self, task_file, server):
+    def format_task_title(self, num):
+        return f'{self.test_task_title}{num}'
+
+    def test_add_task(self, task_file, server):
         response = requests.get(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}/tasks")
         assert response.status_code == 200
         tasks = response.json()["tasks"]
         assert len(tasks) == 0, f"Expected empty task list, got {len(tasks)}."
 
-    def test_add_task(self, task_file, server):
-        response = requests.post(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}/tasks", data={"title": self.test_task_title})
+        response = requests.post(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}/tasks", data={"title": self.format_task_title(0)})
         assert response.status_code == 200
 
-    def test_get_added_task(self, task_file, server):
         response = requests.get(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}/tasks")
         assert response.status_code == 200
         tasks = response.json()["tasks"]
         assert len(tasks) == 1, f"Expected task list with 1 entry, got {len(tasks)}."
-        assert tasks[0]["title"] == self.test_task_title, f"Task has incorrect title, expected {self.test_task_title}, got {tasks[0]["title"]}."
+        assert tasks[0]["title"] == self.format_task_title(0), f"Task has incorrect title, expected {self.format_task_title(0)}, got {tasks[0]["title"]}."
         assert tasks[0]["complete"] == False, f"Newly created task is marked as complete."
+
+    def test_add_task_no_title(self, task_file, server):
+        response = requests.post(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}/tasks")
+        assert response.status_code == 400, f"Adding task with no title was allowed."
+
+        response = requests.get(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}/tasks")
+        tasks = response.json()["tasks"]
+        assert len(tasks) == 1, f"Expected task list with 1 entry, got {len(tasks)}."
+
+    def test_add_several_tasks(self, task_file, server):
+        start_ts = time.monotonic()
+        for ii in range(1, 1000):
+            response = requests.post(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}/tasks", data={"title": self.format_task_title(ii)})
+            assert response.status_code == 200
+        delta_t = (time.monotonic() - start_ts)
+        assert delta_t < 1, f"1000 post requests took longer than 1 second."
+
+        response = requests.get(f"{DEFAULT_SERVER_URL}:{DEFAULT_SERVER_PORT}/tasks")
+        assert response.status_code == 200
+        tasks = response.json()["tasks"]
+        assert len(tasks) == 1000, f"Expected task list with 1000 entries, got {len(tasks)}."
+        for ii in range(len(tasks)):
+            assert tasks[ii]["title"] == self.format_task_title(ii), f"Task has incorrect title, expected {self.format_task_title(ii)}, got {tasks[ii]["title"]}."
+            assert tasks[ii]["complete"] == False, f"Newly created task is marked as complete."
 
